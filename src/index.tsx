a32fb0b313d7614e35271528aab811956518b653
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2022-09-12 22:12:30 +0900

    Finished making react tutorial tic-tac-toe

---
+++ src/index.tsx
@@ -0,0 +1,155 @@
+import React from 'react';
+import { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+type SquareType = "O" | "X" | null;
+
+type SquareProps = {
+    value: SquareType;
+    onClick: () => void
+}
+
+
+/* Square */
+const Square: React.FC<SquareProps> = ({ value, onClick }) => {
+    return <button className="square" onClick={() => onClick()}>
+        {value}
+    </button>
+}
+
+type BoardProps = {
+    squares: Array<SquareType>;
+    onClick: (i: number) => void;
+}
+
+/* Board */
+const Board: React.FC<BoardProps> = ({ squares, onClick }) => {
+
+
+    const renderSquare = (i: number) => {
+        return <Square value={squares[i]} onClick={() => onClick(i)} />
+    }
+
+    // const winner = calculateWinner(squares);
+    // let status: string;
+    // if (winner) {
+    //     status = "Winner: " + winner;
+    // } else {
+    //     status = "Next Player: " + (xIsNext ? "X" : "O");
+    // }
+
+    return (
+        <div>
+            <div className="board-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    );
+}
+
+function calculateWinner(squares: Array<SquareType>) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+type HistoryData = {
+    squares: Array<SquareType>;
+}
+
+/* Game */
+const Game: React.FC = () => {
+    const [history, setHistory] = useState<Array<HistoryData>>([
+        { squares: Array(9).fill(null) }
+    ]);
+    const [stepNumber, setStepNumber] = useState<number>(0);
+    const [xIsNext, setXIsNext] = useState<boolean>(true);
+
+
+    const handleClick = (i: number) => {
+        const _history = history.slice(0, stepNumber + 1);
+        const current = _history[_history.length - 1]
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = xIsNext ? "X" : "O";
+
+        setHistory(_history.concat([{ squares: squares }]))
+        setStepNumber(_history.length);
+        setXIsNext(!xIsNext);
+    }
+
+    const jumpTo = (step: number) => {
+        setStepNumber(step);
+        setXIsNext(step % 2 === 0);
+    }
+
+    const _history = history;
+    const current = _history[stepNumber];
+    const winner = calculateWinner(current.squares);
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Next player: " + (xIsNext ? "X" : "O");
+    }
+
+    const moves = history.map((step, move) => {
+        // step: state of squares
+        // move: 手番
+        // decs: decsription
+
+        const desc = move ? "Go to move #" + move : "Go to game Start";
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{desc}</button>
+            </li>
+        );
+    });
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board squares={current.squares} onClick={(i) => handleClick(i)} />
+            </div>
+            <div className='game-info'>
+                <div>
+                    {status}
+                </div>
+                <ol>
+                    {moves}
+                </ol>
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(<Game />, document.getElementById("root"))
